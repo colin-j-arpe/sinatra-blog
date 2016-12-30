4057349a3d06b055c28a905f173e5ac6ef4de435
Colin Arpe <colin.j.arpe@outlook.com>
2016-12-30 03:39:46 -0500

    Final tweaks, layout of post page, comments in JS file

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+// Animate opening post divs on home page
 	posts = $(".unit-container");
 	for (let i = 0; i < posts.length; i++) {
 		$(".unit-container").eq(i).on ("click", function ()	{
@@ -9,6 +10,7 @@ $(document).ready(function () {
 		});
 	}
 
+// Disable login_name field if "use email" is checked
 	$("#login-email-checkbox").on ("click", function ()	{
 		if ($("#login-email-checkbox").prop("checked"))	{
 			$("#login-name-input").attr("disabled", "true");
@@ -17,10 +19,10 @@ $(document).ready(function () {
 		}
 	});
 
+// Put cursor in first field of each modal
 	for (let i = 0; i < $(".modal").length; i++) {
 		$(".modal").eq(i).on ("shown.bs.modal", function () {
 			$(".first-field").eq(i).focus ();
 		});
 	}
 });
-
